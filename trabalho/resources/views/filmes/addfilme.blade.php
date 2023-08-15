4c278ba0076b28cbf6273c6afdbb6588ff840b2b
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-08-15 17:03:04 -0300

    o

---
+++ trabalho/resources/views/filmes/addfilme.blade.php
@@ -0,0 +1,25 @@
+@extends('includes.base')
+
+@section('title', 'Filmes - Adicionar')
+
+@section('content')
+    <h2>Adicione seu produto</h2>
+
+    @if ($errors)
+        @foreach ($errors->all() as $err)
+            {{ $err }}<br>
+        @endforeach
+    @endif
+
+    <form action="{{ url()->current() }}" method="post">
+
+        @csrf
+        <input type="text" name="name" placeholder="Nome do produto" value="{{ old('name', $prod->name ?? '') }}">
+        <br>
+        <input type="number" name="price" step="0.01" placeholder="Preço" min="0" value="{{ old('price', $prod->price ?? '') }}">
+        <br>
+        <input type="number" name="quantity" placeholder="Quantidade" min="0" value="{{ old('quantity', $prod->quantity ?? '') }}">
+        <hr width="30%" align="left">
+        <input type="submit" value="Gravar">
+    </form>
+@endsection
---
+++ trabalho/resources/views/includes/base.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <h1>Site de filmes lindão</h1>
+    <!-- Menu -->
+    <div>
+        <ul>
+            <li><a href="{{ route('home')}} ">Home</a></li>
+            <li><a href="{{ route('filmes')}}">Filmes</a></li>
+        </ul>
+    </div>
+    <hr style="height:6px;background-color:deepskyblue">
+
+    {{-- Conteúdo --}}
+    @yield('content')
+
+</body>
+</html>
