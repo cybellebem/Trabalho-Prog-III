f99ba3cd233f7b6576826ace4fbe68876a9f60e4
Cybelle Bem <cybellegbem@gmail.com>
2023-08-15 19:23:11 -0300

    banco pt2

--- trabalho/database/migrations/2023_08_15_220650_create_filme_table.php
+++ trabalho/database/migrations/2023_08_15_220650_create_filme_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('filme', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin');
-            $table->rememberToken();
+            $table->string('sinopse', 1000);
+            $table->year('ano');
+            $table->string('imagem');
+            $table->string('link');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('filme');
     }
 };
--- trabalho/database/migrations/2023_08_15_220904_create_generofilme_table.php
+++ trabalho/database/migrations/2023_08_15_220904_create_generofilme_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('generofilme', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin');
-            $table->rememberToken();
+            $table->foreignId('filme_fk')->constrained(
+                table: 'filme'
+            );
+            $table->foreignId('genero_fk')->constrained(
+                table: 'genero'
+            );
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('generofilme');
     }
 };
