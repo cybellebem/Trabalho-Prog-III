2b04dbb0202576e96a4a81b6909d7f8d41eb296b
Cybelle Bem <cybellegbem@gmail.com>
2023-08-16 21:59:09 -0300

    detalhes

--- trabalho/resources/views/filmes/infofilme.blade.php
+++ trabalho/resources/views/filmes/infofilme.blade.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <title>Listagem</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/lista.css') }}" rel="stylesheet">
+    <title>Detalhes do Filme</title>
 </head>
 <body>
-
-<div class="film-details">
-        <h2>{{ $filme->name }}</h2>
-        <img src="{{ asset('storage/' . $filme->imagem) }}" alt="{{ $filme->name }}">
-        <p>Sinopse: {{ $filme->sinopse }}</p>
-        <p>Ano: {{ $filme->ano }}</p>
-        <a href="{{$filme->link}}">link</a>
-        <!-- Adicione mais informações aqui -->
+<div class="header">
+        <h1>Detalhes do Filme</h1>
+        <nav>
+            <a href="{{ route('lista') }}">Voltar à Lista</a>
+        </nav>
+    </div>
+    <div class="container">
+        <div class="film-details">
+            <img src="{{ asset('storage/' . $filme->imagem) }}">
+            <h2>{{ $filme->name }}</h2>
+            <p><strong>{{ $filme->ano }}</strong></p>
+            <p class="sinopse">{{ $filme->sinopse }}</p>
+            <a href="{{$filme->link}}" target="_blank">Assistir ao Trailer</a>
+        </div>
     </div>
 </body>
 </html>
